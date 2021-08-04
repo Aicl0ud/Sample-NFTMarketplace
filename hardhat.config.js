7077ad6e83f435becd9e589c3b0d070c0471bd2f
kiw1 <kiw@nuxos.asia>
2021-08-04 12:03:54 +0700

    init contract

---
+++ hardhat.config.js
@@ -0,0 +1,34 @@
+/* hardhat.config.js */
+require("@nomiclabs/hardhat-waffle")
+const fs = require('fs')
+const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [privateKey]
+    },
+    matic: {
+      url: "https://rpc-mainnet.maticvigil.com",
+      accounts: [privateKey]
+    },
+    bsc: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: [privateKey]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
